ba82d5bc38ae7f9a7874622587dec87d13e65123
Danielgpr <danielg@continuumhealthtech.com>
2025-02-18 17:47:13 +0200

    Changingvars to let

--- src/styles/ColorUtils.ts
+++ src/styles/ColorUtils.ts
@@ -5,7 +5,7 @@ function hex2(c: number) {
   if (c < 0) c = 0;
   if (c > 255) c = 255;
 
-  var s = c.toString(16);
+  let s = c.toString(16);
   if (s.length < 2) s = "0" + s;
 
   return s;
@@ -18,9 +18,9 @@ export function hexColor(r: number, g: number, b: number) {
 export function shadeColor(color: string, light: number) {
   // TODO: Assert that col is good and that -1 < light < 1
 
-  var r = parseInt(color.substr(1, 2), 16);
-  var g = parseInt(color.substr(3, 2), 16);
-  var b = parseInt(color.substr(5, 2), 16);
+  let r = parseInt(color.substring(1, 2), 16);
+  let g = parseInt(color.substring(3, 2), 16);
+  let b = parseInt(color.substring(5, 2), 16);
 
   if (light < 0) {
     r = (1 + light) * r;
